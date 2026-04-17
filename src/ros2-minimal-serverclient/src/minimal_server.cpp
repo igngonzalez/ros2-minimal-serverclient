8f69fe96f875cd12adbaffa377d3bb93c86fa128
igngonzalez <ignacio.javier.gonzalez@fing.edu.uy>
2026-04-17 14:56:42 -0300

    fix: correct namespace qualifiers in minimal_server.cpp

--- src/ros2-minimal-serverclient/src/minimal_server.cpp
+++ src/ros2-minimal-serverclient/src/minimal_server.cpp
@@ -7,12 +7,12 @@ class MinimalServer : public rclcpp::Node { // Inherits from class Node from rcl
         rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr server_;    // Shared pointer to a service object of type AddTwoInts from example_interfaces
         void server_callback(const example_interfaces::srv::AddTwoInts::Request::SharedPtr req, const example_interfaces::srv::AddTwoInts::Response::SharedPtr resp) { // Callback function. As we declared in the server constructor, it has 2 parameters (placeholders), the request and the response
             resp->sum = req->a + req->b;
-            RCPCPP_INFO(this->get_logger(), "Received request: a=%d, b=%d", (int)req->a, (int)req->b);  // Prints an informational message in the ROS2 console log.
+            RCLCPP_INFO(this->get_logger(), "Received request: a=%d, b=%d", (int)req->a, (int)req->b);  // Prints an informational message in the ROS2 console log.
         }
 
     public:
         MinimalServer() : Node("minimal_server") {
-            server_ = this -> create_service<example_interfaces::srv::AddTwoInts>("add_ints", std::bind(&MinimalServer::server_callback, this, std::placeholders::_1, std::placeholders_2));    // Create a service object. This is what creates our node into a server.
+            server_ = this -> create_service<example_interfaces::srv::AddTwoInts>("add_ints", std::bind(&MinimalServer::server_callback, this, std::placeholders::_1, std::placeholders::_2));    // Create a service object. This is what creates our node into a server.
                                                                                                                                                                                                 // We do this by calling the function create_service from Node.
                                                                                                                                                                                                 // <Type_for_the_template>(name_of_our_service, which_callback_should_be_used)
     
